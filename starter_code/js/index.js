9045e4a9d1f61fdb6e6cbd46ae839ac555aa114c
Karen McCann <kare.mc@live.co.uk>
2015-10-14 20:08:40 +0100

    done

--- starter_code/js/index.js
+++ starter_code/js/index.js
@@ -8,7 +8,7 @@ var city = ['NYC', 'LA', 'SF', 'ATX', 'SYD'];
   for (var i = 0; i < 5; i++) {
    //console.log(city[i]);
    // append the city as option for dropdown
-   $('#city-type').append('<option>' + city[i] + '<option>');
+   $('#city-type').append('<option>' + city[i] + '</option>');
  }
 
 //user enters city name
@@ -16,13 +16,13 @@ var city = ['NYC', 'LA', 'SF', 'ATX', 'SYD'];
 // update the background based on the users input
     $('#city-type').change(function(){
     var city = $('#city-type').val()
-     
+    
       //if user submits "New York" or "New York City" or "NYC" make the background of the page nyc.jpg
     if (city == "NYC"){
       //KEPT THIS IN FOR PERSONAL NOTES, SHOWING THERES ANOTHER WAY!
       //$('body').css("background", "url(images/nyc.jpg) no-repeat center center fixed");
       $("body").attr("class", "nyc");
-
+      
       //if user submits "San Francisco" or "SF" or "Bay Area" make the background of the page sf.jpg
     } else if (city == "SF") {
       //KEPT THIS IN FOR PERSONAL NOTES, SHOWING THERES ANOTHER WAY
